a298ee7baf6bef3029b2417f50c2c090d5a7aaa0
Local Developer <local@dev.com>
2026-09-06 19:59:43 +0530

    Local save: Dashboard working with real members data

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
+  },
+  allowedDevOrigins: ["192.168.0.10:3000", "192.168.0.10", "localhost:3000"],
+  output: "export",
+  ...(isProd && {
+    basePath: "/v2",
+    assetPrefix: "/v2",
+  }),
+  images: {
+    unoptimized: true,
   },
 };
 
